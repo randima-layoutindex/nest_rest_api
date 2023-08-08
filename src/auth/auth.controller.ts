b2a92fdc6e006c8736ba48a307105c902786be60
randima-layoutindex <randima.layoutindex@gmail.com>
2023-08-08 17:36:10 +0530

    1st day office

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post,Req ,Body,ParseIntPipe} from '@nestjs/common';
 import { AuthService } from './auth.service';
+// import {Request} from "express"
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
@@ -7,7 +9,18 @@ export class AuthController {
     // this.authService.test();
   }
   @Post('signup')
-  signup() {
+  // signup(@Body() dto:any) {
+  signup(@Body() dto:AuthDto) {
+    console.log(dto)
+  // signup(@Req() req:Request) {
+    // console.log(req.body)
+    // signup(@Body("email") email:string,@Body("password",ParseIntPipe) password:string){
+    // console.log({
+    //   email,
+    //   typeOfEmail:typeof email,
+    //   password,
+    //   typeOfPassword:typeof password
+    // })
     return this.authService.signup();
   }
   @Post('signin')
